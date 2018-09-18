ca99c1d1b01a7981b1264da3c87956dfd15a40b8
bear-dog-lover <lagrange.resolvent@gmail.com>
2018-09-18 11:52:44 +0900

    P.102 - 2

--- part1/多国通貨/src/money/Money.java
+++ part1/多国通貨/src/money/Money.java
@@ -31,6 +31,7 @@ class Money implements Expression {
 		return new Sum(this, addend);
 	}
 	public Money reduce(String to) {
-		return this;
+		int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+		return new Money(amount / rate, to);
 	}
 }
