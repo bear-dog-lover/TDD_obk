e0f29777726ff2fb21343855e3f8de9f56a10fe7
bear-dog-lover <lagrange.resolvent@gmail.com>
2018-09-18 11:35:47 +0900

    P.94 - 1

--- part1/多国通貨/src/money/Bank.java
+++ part1/多国通貨/src/money/Bank.java
@@ -1,11 +1,8 @@
 package money;
 
 class Bank {
-
-	public Money reduce(Expression source, String to) {
+	Money reduce(Expression source, String to) {
 		Sum sum = (Sum) source;
-		int amount = sum.augend.amount + sum.addend.amount;
-		return new Money(amount, to);
+		return sum.reduce(to);
 	}
-
 }
